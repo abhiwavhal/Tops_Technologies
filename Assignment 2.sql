ceddd4ced055b94cfe756a770a2634fb9c36dc4a
Abhishek Sudarshan Wavhal <127376950+abhiwavhal@users.noreply.github.com>
2023-10-21 16:32:34 +0530

    Add files via upload

--- Assignment 2.sql
+++ Assignment 2.sql
@@ -27,20 +27,60 @@ order by first_name;
 
 /*6) Display the employees name, department id and commission. If an Employee doesn't 
 earn the commission, then display as 'No commission'. Name the columns appropriately*/
-
+SELECT
+    employee.empname,
+    department.deptid,
+    CASE
+        WHEN employee.commission > 0 THEN employee.commission
+        ELSE 'No commission'
+    END AS commission
+FROM
+    employee 
+LEFT JOIN
+    department  ON employee.empid = department.deptid;
 
 
 /*7) Display the employee's name, salary and commission multiplied by 2. If an Employee 
 doesn't earn the commission, then display as 'No commission. Name the columns 
 appropriately*/
-/*8) Display the employee's name, department id who have the first name same as another 
-employee in the same department*/
+SELECT
+    empname,salary,
+    CASE
+        WHEN commission > 0 THEN commission * 2 
+        ELSE 'No commission'
+    END AS commission
+FROM employee ;
+
 /*9) Display the sum of salaries of the employees working under each Manager. */
+
+select sum(salary),managername from employee
+group by managername;
+
 /*10) Select the Managers name, the count of employees working under and the department 
 ID of the manager. */
+
+select employee.managername,count(employee.empname),department.deptname from
+department join employee on department.deptid = employee.empid
+group by department.deptname;
+
 /*11) Select the employee name, department id, and the salary. Group the result with the 
 manager name and the employee last name should have second letter 'a! */
-/*12) Display the average of sum of the salaries and group the result with the department id. 
-Order the result with the department id. */
+
+select employee.empname,department.deptid,employee.salary,managername from
+employee join department on department.deptid = employee.empid
+where employee.managername like '_a%' and employee.emplastname like '_a%';
+
 /*13) Select the maximum salary of each department along with the department id */
+
+select max(employee.salary),department.deptname from employee
+join department on department.deptid = employee.empid
+group by department.deptname;
+
 /*14) Display the commission, if not null display 10% of salary, if null display a default value 1*/
+
+select empname,salary,
+case
+when commission > 0 then salary * 0.1
+else '1'
+end as commission
+from employee;
